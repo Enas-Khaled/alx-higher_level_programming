ceeed62f8e3c7b8782349b66b3d9db5d2439807d
Enas Khaled <enas.ah.khaled@gmail.com>
2023-11-20 13:03:49 -0800

    fix

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <time.h>
 #include <stdlib.h>
-#include <Python.h>
+//#include <Python.h>
 void print_python_list_info(PyObject *p);
 #endif /* LISTS_H */
