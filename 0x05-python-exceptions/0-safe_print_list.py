d9e0116515cc1b9d8df7227dc913da70cd5b0c30
Enas Khaled <enas.ah.khaled@gmail.com>
2023-11-20 10:15:05 -0800

    Index Error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-        for i in my_list:
-            print(i, end="")
-    except Exception as e:
+        while i is not x:
+            print(my_list[i], end="")
+    except IndexError:
         None
     finally:
-        print('\n')
+        print()
     return i
