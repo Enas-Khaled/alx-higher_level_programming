17702657d3a0219a28a208447d720a7408420787
Enas Khaled <enas.ah.khaled@gmail.com>
2023-11-20 10:53:36 -0800

    type Error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except TypeError:
-        return 0
+    except:
+        return False
     else:
-        return 1
+        return True
