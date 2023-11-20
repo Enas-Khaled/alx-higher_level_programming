0f3aa1d996d84562ec23ee76f26e89c167af1de1
Enas Khaled <enas.ah.khaled@gmail.com>
2023-11-20 08:03:32 -0800

    test error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-try:
-    def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
         for i in my_list:
             print(i, end="")
+    except Exception as e:
+        None
+    finally:
         print('\n')
-except Exception as e:
-    print("",end="")
+    return i
