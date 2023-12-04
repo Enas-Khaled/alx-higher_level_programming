bf7deb8df99e55a17b09b0831cac48747a6c1770
Enas Khaled <enas.ah.khaled@gmail.com>
2023-12-04 14:59:49 -0800

    test

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for BaseGeometry class.'''
+
+
+class BaseGeometry:
+    '''A BaseGeometry class.'''
+    def area(self):
+        raise Exception("area() is not implemented")
