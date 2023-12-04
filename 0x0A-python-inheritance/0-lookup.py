9f631aaa1ce6df5913c419dac7567afab68adbe8
Enas Khaled <enas.ah.khaled@gmail.com>
2023-12-04 13:47:24 -0800

    dir()

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module t return  available attributes and methods of an object"""
+
+
+def lookup(obj):
+    '''
+    Prototype: def lookup(obj):
+    Returns a list object
+    '''
+
+    return dir(obj)
