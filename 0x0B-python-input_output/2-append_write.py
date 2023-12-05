3ebe9241742e5b001d010c2194c540f60d134cad
Enas Khaled <enas.ah.khaled@gmail.com>
2023-12-05 15:15:39 -0800

    test

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """append txt"""
 
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
