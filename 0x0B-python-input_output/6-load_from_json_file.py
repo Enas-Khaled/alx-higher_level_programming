54bff0c61ee4acdd1af961a2c91970996356f1d0
Enas Khaled <enas.ah.khaled@gmail.com>
2023-12-06 02:45:10 -0800

    load from json f

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+'''load from json'''
 
 import json
 
 
 def load_from_json_file(filename):
+    '''load from json'''
+
     with open(filename, 'r', encoding="utf-8") as f:
         data = json.load(f)
         return data
-
